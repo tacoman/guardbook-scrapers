080bfcb09eb29498a68b310d0644d622a6d7dac5
Sam Shrum <Sam.L.Shrum@gmail.com>
2021-11-13 13:46:18 -0500

    Reading out the data

--- uslc.py
+++ uslc.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
 driver.get("https://www.uslchampionship.com/league-teams")
@@ -11,4 +12,26 @@ for url in rosterUrls:
     driver.get(url)
     teamInfo = driver.find_element(By.CLASS_NAME, "teamInfo")
     print(teamInfo.find_element(By.TAG_NAME, "h1").text)
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "Opta-Team"))
+    )
+    rosterTable = driver.find_element(By.CLASS_NAME, "Opta-Team")
+    rosterRows = rosterTable.find_elements(By.TAG_NAME, "tr")
+    currentPosition = ""
+    for row in rosterRows:
+        classList = row.get_attribute("class")
+        if "Opta-Position" in classList:
+            if(row.text == "Goalkeeper"):
+                currentPosition = "GK"
+            if(row.text == "Defender"):
+                currentPosition = "D"
+            if(row.text == "Midfielder"):
+                currentPosition = "M"
+            if(row.text == "Forward"):
+                currentPosition = "F"
+        if "Opta-Player" in classList:
+            shirtNumber = row.find_element(By.CLASS_NAME, "Opta-Shirt").text
+            print(shirtNumber)
+            name = row.find_element(By.CLASS_NAME, "Opta-Name").text
+            print(name)
 driver.close()
