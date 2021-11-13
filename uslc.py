543a41486cefe512a28d614d97adf2c77781ead4
Sam Shrum <Sam.L.Shrum@gmail.com>
2021-11-13 17:29:06 -0500

    Grab color data from website

--- uslc.py
+++ uslc.py
@@ -2,12 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.color import Color
+import json
 
 driver = webdriver.Firefox()
 driver.get("https://www.uslchampionship.com/league-teams")
 rosterLinks = driver.find_elements(By.XPATH, "//*[text()='Roster']")
 rosterUrls = [x.get_attribute("href") for x in rosterLinks]
 print(rosterUrls)
+foes = []
 for url in rosterUrls:
     driver.get(url)
     teamInfo = driver.find_element(By.CLASS_NAME, "teamInfo")
@@ -18,6 +21,15 @@ for url in rosterUrls:
     rosterTable = driver.find_element(By.CLASS_NAME, "Opta-Team")
     rosterRows = rosterTable.find_elements(By.TAG_NAME, "tr")
     currentPosition = ""
+    foe = {}
+    foe["players"] = []
+    foe["opponent"] = teamInfo.find_element(By.TAG_NAME, "h1").text
+    foe["competition"] = "USL Championship"
+    foe["textColor"] = "#000000"
+    foe["backgroundColor"] = "#ffffff"
+    banner = driver.find_element(By.CLASS_NAME, "teamInfoBanner")
+    bannerColor = banner.value_of_css_property("background-color")
+    foe["accentColor"] = Color.from_string(bannerColor).hex
     for row in rosterRows:
         classList = row.get_attribute("class")
         if "Opta-Position" in classList:
@@ -30,8 +42,15 @@ for url in rosterUrls:
             if(row.text == "Forward"):
                 currentPosition = "F"
         if "Opta-Player" in classList:
-            shirtNumber = row.find_element(By.CLASS_NAME, "Opta-Shirt").text
-            print(shirtNumber)
-            name = row.find_element(By.CLASS_NAME, "Opta-Name").text
-            print(name)
+            player = {}
+            player["squadNumber"] = row.find_element(By.CLASS_NAME, "Opta-Shirt").text
+            player["name"] = row.find_element(By.CLASS_NAME, "Opta-Name").text
+            player["position"] = currentPosition
+            foe["players"].append(player)
+    foes.append(foe)
+
 driver.close()
+
+foeJSON = json.dumps(foes, ensure_ascii=False)
+with open('foes.json', 'w') as f:
+    f.write(foeJSON)
